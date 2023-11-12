8a9077bf6674486dd1acb8aae59a1de1d2a6c437
harshahossain <harshahossain@gmail.com>
2023-11-12 23:29:59 +0600

    Used ref for Result

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,22 @@
-export default function ResultModal({ result, targetTime }) {
+import { forwardRef } from "react";
+//for this forward ref (see more in TimeChallenge comp for info)
+//for this to use I have to wrap it around the whole component
+
+//after  wraping we dont need to destructr the ref anymore but it will recieve a second arg after the props arg
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
   //when we set the form method to dialog and add a button at the end of it by default it closes the page.. this is vanilla
   //we need to add the open prop otherwise it doesnt come live because by default the visibility is false and open trues it???
+
   //for the backdrop we need to open the dialog programmatically and thats again where ref will help us
+  //for that we need to control the Ref on the TimerChallenge.jsx
+
+  //now here im using special ref prop. back in TimeChallenge i was just passing it as prop so the name wasnt important
   return (
-    <dialog className="result-modal" open>
-      <h2>You {result}!</h2>
+    <dialog ref={ref} className="result-modal">
+      <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
@@ -16,4 +28,6 @@ export default function ResultModal({ result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
+
+export default ResultModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -6,12 +6,14 @@ export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
 
   const timer = useRef();
+  const dialog = useRef();
 
   //let timer; state changes make this component re-execute so the timer is taken new.. We need to use Ref instead
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal(); //showModal() is built in native html. we could use use show()
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -23,7 +25,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+      {/* the name is upto me(cause im passing it as prop not as special obj ref?) but for easier understanding im setting it to ref in resultmodal . however its important to set the prop to the useRef obj*/}
+      {/* howevber this shows a  problem that ref cant be prop so we  gotta import forwardRef from react in the comp that im passing the ref */}
+      {/* ++in ResultModal */}
+      {/* but now after changes in resultModal it should also be called ref so react recongnizes  it */}
+      {/* and we finally get the backdrop */}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         {timerExpired && <p>You Lost!</p>}
