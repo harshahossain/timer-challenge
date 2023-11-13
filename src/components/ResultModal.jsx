48e8a820331df3c1c17f69427467de02899b0feb
harshahossain <harshahossain@gmail.com>
2023-11-13 14:07:13 +0600

    Updated

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,12 +1,13 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 //for this forward ref (see more in TimeChallenge comp for info)
 //for this to use I have to wrap it around the whole component
 
 //after  wraping we dont need to destructr the ref anymore but it will recieve a second arg after the props arg
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { onReset, targetTime, remainingTime },
   ref
 ) {
+  const dialog = useRef(); //cause now  we need a separate ref reaching out to the dialog because we wanna deatch it for more usability
   //when we set the form method to dialog and add a button at the end of it by default it closes the page.. this is vanilla
   //we need to add the open prop otherwise it doesnt come live because by default the visibility is false and open trues it???
 
@@ -14,16 +15,35 @@ const ResultModal = forwardRef(function ResultModal(
   //for that we need to control the Ref on the TimerChallenge.jsx
 
   //now here im using special ref prop. back in TimeChallenge i was just passing it as prop so the name wasnt important
+
+  //useImperativeHandle hook just makes it impartive so we can changle the dialog to div and it will still work
+  //we cann call this hook inside comp fn to define properties and method that should be accessible on this component here from outside this component. i.e. dialog current showmodal from timerchallenge
+  //it uses 2 args and first one is always the ref which we got from forward ref
+  //and the 2nd value is a function  that groups all the properties and methods that should be exposed by this component to other component
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        //the names upto me
+        dialog.current.showModal();
+      },
+    };
+  });
+  //so when open() which i named is called, the showModal () on this dialog
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
-    <dialog ref={ref} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You Lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,25 +2,45 @@ import React, { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 //let timer;
 export default function TimerChallenge({ title, targetTime }) {
-  const [timerExpired, setTimerExpired] = useState(false);
-  const [timerStarted, setTimerStarted] = useState(false);
+  // const [timerExpired, setTimerExpired] = useState(false);
+  // const [timerStarted, setTimerStarted] = useState(false);
 
   const timer = useRef();
   const dialog = useRef();
 
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    //setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
   //let timer; state changes make this component re-execute so the timer is taken new.. We need to use Ref instead
 
+  // function handleStart() {
+  //   timer.current = setTimeout(() => {
+  //     setTimerExpired(true);
+  //     dialog.current.open(); //showModal() is built in native html. we could use use show()
+  //   }, targetTime * 1000);   //we are calling showModal() inside ResultModal now
+  //   setTimerStarted(true);
+  // }
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.showModal(); //showModal() is built in native html. we could use use show()
-    }, targetTime * 1000);
-    setTimerStarted(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevState) => prevState - 10);
+    }, 10);
+    //setTimerStarted(true);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
-    setTimerStarted(false);
+    //clearTimeout(timer.current);
+    //setTimerStarted(false);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
@@ -30,20 +50,25 @@ export default function TimerChallenge({ title, targetTime }) {
       {/* ++in ResultModal */}
       {/* but now after changes in resultModal it should also be called ref so react recongnizes  it */}
       {/* and we finally get the backdrop */}
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You Lost!</p>}
+        {/* {timerIsActive && <p>You Lost!</p>} */}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop " : "Start "}Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop " : "Start "}Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
